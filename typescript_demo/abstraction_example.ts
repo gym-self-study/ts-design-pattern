1e299168510e1ab45e761d3b2e2271d71d1fdab5
Yiming0576 <Yiming0576@gmail.com>
2024-11-10 20:12:52 -0500

    change the file name and add new content.

--- typescript_demo/abstraction_example.ts
+++ typescript_demo/abstraction_example.ts
@@ -52,10 +52,14 @@ function printShape(shape: Shape) {
     shape.print?.();
 }
 
+// example of here we are using the Shape interface to calculate the area and perimeter of different shapes.
+// we are not sure about the implementation of the Circle, Square, and Retangle classes, but we know that they will return the area and perimeter of the shapes.
 function calculateArea(shape: Shape) {
     return shape.getArea();
 }
 
+// example of here we are using the Shape interface to calculate the area and perimeter of different shapes.
+// we are not sure about the implementation of the Circle, Square, and Retangle classes, but we know that they will return the area and perimeter of the shapes.
 function calculatePerimeter(shape: Shape) {
     return shape.getPerimeter();
 }
@@ -72,18 +76,16 @@ printShape(retangle);
 console.log("Circle Area: " + calculateArea(circle));
 console.log("Square Area:"  + calculateArea(square));
 console.log( "Retangle Area:" + calculateArea(retangle));
-
 console.log("Circle Perimeter:" + calculatePerimeter(circle));
 console.log("Square Perimeter:" + calculatePerimeter(square));
 console.log( "Retangle Perimeter:" + calculatePerimeter(retangle));
 
 
-// Date
+// Date   
+// example of here we are using the Date class to get the current date, month, and year.
+// we are not sure about the implementation of the Date class, but we know that it will return the current date, month, and year.
 const date = new Date();
-
-
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
-
 console.log(`Date: ${month}/${day}/${year}`); // Date: 9/15/2021
